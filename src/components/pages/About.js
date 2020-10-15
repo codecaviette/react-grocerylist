40ddfd6a69ca844aabe5852b8b491b08845afb8a
codecaviette <63386589+codecaviette@users.noreply.github.com>
2020-10-14 21:00:08 -0700

    Update About Page Title

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
     return (
         <React.Fragment>        {/* If you don't need a div tag, you can use this "ghost element" which won't actually show in DOM */}
-            <h1> About </h1>
+            <h1> About Page </h1>
             <p> This is the GroceryList app v1.0.0. </p>
         </React.Fragment>
     )
